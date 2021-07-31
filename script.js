69875efe56d9ea4f5ce1af134dbb3b25af9fa6f6
Alex Edwards <anedwards180@gmail.com>
2021-07-31 18:07:55 -0500

    Nav Pages created

---
+++ script.js
@@ -0,0 +1,9 @@
+face.addEventListener('mouseover',function () {
+    face.style.width = '200px';
+    face.style.height = '200px';
+
+}); 
+
+face.addEventListener('mouseleave', function(){
+    face.style.width = '50px';
+    face.style.height = '50px';});
