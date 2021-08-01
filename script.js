24e8ff7d6fed21e0b05e8a0c0a554ead848c7c9e
Alex Edwards <anedwards180@gmail.com>
2021-08-01 03:04:20 -0500

    Added contact page and hover over

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-face.addEventListener('mouseover',function () {
-    face.style.width = '200px';
-    face.style.height = '200px';
+document.getElementById("pfa1").addEventListener("click", function() {
+    alert(`Psychological First Aid, or "PFA", is a mental health tool that can be used by anyone to help individuals experiencing stress and  trauma. 
+    The World Health Organization offers free PFA certifications online. Check out their website WHO.int for more details.`);})
 
-}); 
 
-face.addEventListener('mouseleave', function(){
-    face.style.width = '50px';
-    face.style.height = '50px';});
+document.getElementById("mhfa1").addEventListener("click", function() {
+    alert(`Mental Health First Aid, or "MHFA", is a mental health tool that differs  from PFA by teaching participants about how to identify various mental health issues, 
+    and helps to provide information about resources in regards to those in need.`);})
